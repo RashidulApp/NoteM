99fa8084054e5e322483e6699b7e1a4f4e50680a
Rashidul Islam <rashidul.app@gmail.com>
2023-06-16 15:55:23 +0600

    crud api v1

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Why we need define Schema ??
+ => defining a schema provides structure, validation, readability, maintainability, and performance benefits when working with a database. It helps you maintain data consistency and integrity while offering flexibility to manipulate and query the data effectively.
+
+
+
+
---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello node js world");
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/note", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb Connected sucssesfully"))
+  .catch((error) => console.log(`mongodb connected fail ${error}`));
+
+const nameSchema = mongoose.Schema({
+  name: String,
+});
+
+const Name = mongoose.model("Name", nameSchema);
+
+app.post("/name", (req, res) => {
+  const newName = new Name(req.body);
+
+  newName
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Data save succesfully" });
+    })
+    .catch((error) => {
+      console.log(`Data not save : ${error}`);
+      res.status(500).json({ error: "an error occurred" });
+    });
+});
+
+app.get("/names", (req, res) => {
+  Name.find()
+    .then((names) => {
+      res.status(200).json(names);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: "Fail to get data in the server",
+      });
+    });
+});
+
+app.get("/name/:id", (req, res) => {
+   const {id} =  req.params
+   Name.findById(id).then((nameId) =>{
+    if(nameId){
+        res.status(200).json(nameId)
+    } else {
+        res.status(400).json({error: "Name not found by id"})
+    }
+   } ).catch((error) => {
+    res.status(500).json({error : `Something was worng in the server ${error}`})
+   })
+
+})
+
+app.put("/name/:id", (req, res) => {
+  const { id } = req.params;
+  const updateName = req.body;
+
+  Name.findByIdAndUpdate(id, updateName, { new: true })
+    .then((updateName) => {
+        if(updateName){
+           res.status(200).json(updateName)
+        }else{
+            res.status(400).json({error: "Name not found"})
+        }
+    })
+    .catch((error) => {
+        res.status(500).json({error: "Fail to update data"})
+    });
+});
+
+
+app.delete("/name/:id", (req, res) => {
+  const {id} =  req.params
+    Name.findByIdAndRemove(id).then((deleteName) => {
+        if(deleteName){
+            res.status(200).json({message: "Successfully delete name"})
+        }else{
+            res.status(400).json({error: "Name not found"})
+        }
+    }).catch((error) => {
+
+        res.status(500).json({error: "Name delete fail"})
+    })
+})
+
+
+app.listen(2000, () => console.log("Server is running Port 2000"));
